e76491bc1760c932cc26cb276b2d87a83f39b502
Nifraz Navahz <nifraz.navahz@cloudsolutions.com.sa>
2023-07-01 15:08:31 +0530

    add graphql

--- Graphql.Demo.API/Program.cs
+++ Graphql.Demo.API/Program.cs
@@ -1,6 +1,15 @@
+using Graphql.Demo.API.Schema;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddGraphQLServer().AddQueryType<Query>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapGraphQL();
+});
 
 app.Run();
---
+++ Graphql.Demo.API/Schema/Query.cs
@@ -0,0 +1,7 @@
+﻿namespace Graphql.Demo.API.Schema
+{
+    public class Query
+    {
+        public string Instructions => "Graphql query is working! :)";
+    }
+}
